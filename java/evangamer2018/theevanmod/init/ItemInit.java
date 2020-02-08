f9a75241e56ab80ea90709068709e4d5be5d14b4
EvanGamer2020 <54504816+EvanGamer2020@users.noreply.github.com>
2020-02-08 12:11:05 -0500

    Finished Adding Infinity Pickaxe

--- java/evangamer2018/theevanmod/init/ItemInit.java
+++ java/evangamer2018/theevanmod/init/ItemInit.java
@@ -1,9 +1,14 @@
 package evangamer2018.theevanmod.init;
 
 import evangamer2018.theevanmod.main;
+import evangamer2018.theevanmod.init.ModItemTier;
 import net.minecraft.item.Foods;
+import net.minecraft.item.IItemTier;
 import net.minecraft.item.Item;
+import net.minecraft.item.PickaxeItem;
+import net.minecraft.tags.Tag;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemTier;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -13,6 +18,8 @@ import net.minecraftforge.registries.ObjectHolder;
 @Mod.EventBusSubscriber(modid = main.MODID, bus = Bus.MOD)
 @ObjectHolder(main.MODID)
 public class ItemInit {
+	
+	// Item Registry
 	public static final Item pancake = null;
 	public static final Item pancake_batter = null;
 	public static final Item infinity_ingot = null;
@@ -22,6 +29,6 @@ public class ItemInit {
 		event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.FOOD).food(Foods.COOKED_BEEF)).setRegistryName("pancake"));
 		event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.FOOD).food(Foods.CARROT)).setRegistryName("pancake_batter"));
 		event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MATERIALS)).setRegistryName("infinity_ingot"));
-		event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.TOOLS).addToolType(net.minecraftforge.common.ToolType.PICKAXE, 3)).setRegistryName("infinity_pickaxe"));
+		event.getRegistry().register(new PickaxeItem(ModItemTier.infinity , 4, -2.0f, new Item.Properties().addToolType(net.minecraftforge.common.ToolType.PICKAXE, 3).group(ItemGroup.TOOLS)).setRegistryName("infinity_pickaxe"));
 	}
 }
---
+++ java/evangamer2018/theevanmod/init/ModItemTier.java
@@ -0,0 +1,69 @@
+package evangamer2018.theevanmod.init;
+
+import java.util.function.Supplier;
+
+import evangamer2018.theevanmod.init.ItemInit;
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.LazyValue;
+
+public enum ModItemTier implements IItemTier 
+{
+    infinity(3, 3000, 14.0F, 1.5F, 30, () -> {
+    	return Ingredient.fromItems(ItemInit.infinity_ingot);
+    });
+    
+    private final int harvestLevel;
+    private final int maxUses;
+    private final float efficiency;
+    private final float attackDamage;
+    private final int enchantability;
+    private final LazyValue<Ingredient> repairMaterial;
+    
+	
+	private ModItemTier(int harvestLevelIn, int maxUsesIn, float efficiencyIn, float attackDamageIn, int enchantabilityIn, Supplier<Ingredient> repairMaterialIn) 
+    {
+        this.harvestLevel = harvestLevelIn;
+        this.maxUses = maxUsesIn;
+        this.efficiency = efficiencyIn;
+        this.attackDamage = attackDamageIn;
+        this.enchantability = enchantabilityIn;
+        this.repairMaterial = new LazyValue<>(repairMaterialIn);
+    }
+
+	@Override
+    public int getMaxUses() 
+    {
+        return this.maxUses;
+    }
+
+    @Override
+    public float getEfficiency() 
+    {
+        return this.efficiency;
+    }
+
+    @Override
+    public float getAttackDamage() 
+    {
+        return this.attackDamage;
+    }
+
+    @Override
+    public int getHarvestLevel() 
+    {
+        return this.harvestLevel;
+    }
+
+    @Override
+    public int getEnchantability() 
+    {
+        return this.enchantability;
+    }
+
+    @Override
+    public Ingredient getRepairMaterial() 
+    {
+        return this.repairMaterial.getValue();
+    }
+}
